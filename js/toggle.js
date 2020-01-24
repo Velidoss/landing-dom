445773a295595cae56115c29823ea5ade799828a
Velidoss <godofthemountain09@gmail.com>
2020-01-24 09:40:45 +0200

    улучшил выпадающее меню

--- js/toggle.js
+++ js/toggle.js
@@ -2,13 +2,19 @@ let toggleNavStatus = false;
 
 let ToggleNav = function() {
   let getToggleBar = document.querySelector(".toggle");
+  let getToggleBarMenu = document.querySelector(".toggle-menu");
+
+  
 
   if (toggleNavStatus === false) {
-    getToggleBar.style.visibility = "visible";
+    getToggleBar.style.height = "200px";
+    getToggleBarMenu .style.visibility = "visible";
+
 
     toggleNavStatus = true;
   } else if (toggleNavStatus === true) {
-    getToggleBar.style.visibility = "hidden";
+    getToggleBar.style.height = "0";
+    getToggleBarMenu .style.visibility = "hidden";
 
     toggleNavStatus = false;
   }
