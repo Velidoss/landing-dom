a2e48e7be2417f7445a72def0f77d7a6629ba847
Velidoss <godofthemountain09@gmail.com>
2020-02-04 19:39:02 +0200

    Подработал меню, сделал вывод постов

---
+++ app/views/Postlist.php
@@ -0,0 +1,21 @@
+<!-- postlist -->
+<section class="section-inner section-postlist">
+    <div class="section-posts-inner">
+        <?php foreach ($data['posts'] as $num => $post) { ?>
+            <div class="section-postlist__post">
+                <div class="section-postlist__post-info">
+                    <img class="section-postlist__post-info-pic" src="/img/anon.png" alt="" />
+                    <p class="section-postlist__post-info-author"><?php echo $post['postAuthor'] ?></p>
+                    <p class="section-postlist__post-info-dateposted"><?php echo $post['postDateTime'] ?></p>
+                </div>
+                <div class="section-postlist__post-content">
+                    <div class="section-postlist__post-content-title"><?php echo $post['postTitle'] ?></div>
+                    <div class="section-postlist__post-content-text">
+                        <?php echo $post['postContent'] ?>
+                    </div>
+                </div>
+            </div>
+        <?php }; ?>
+    </div>
+</section>
+<!-- postlist -->
--- js/toggle.js
+++ js/toggle.js
@@ -1,20 +1,21 @@
 let toggleNavStatus = false;
 
+let toggleBarHeight =
+  document.querySelectorAll(".section-header__top-nav-menu li").length * 64;
+console.log(toggleBarHeight);
+
 let ToggleNav = function() {
   let getToggleBar = document.querySelector(".toggle");
   let getToggleBarMenu = document.querySelector(".toggle-menu");
 
-  
-
   if (toggleNavStatus === false) {
-    getToggleBar.style.height = "280px";
-    getToggleBarMenu .style.visibility = "visible";
-
+    getToggleBar.style.height = toggleBarHeight + "px";
+    getToggleBarMenu.style.visibility = "visible";
 
     toggleNavStatus = true;
   } else if (toggleNavStatus === true) {
     getToggleBar.style.height = "0";
-    getToggleBarMenu .style.visibility = "hidden";
+    getToggleBarMenu.style.visibility = "hidden";
 
     toggleNavStatus = false;
   }
